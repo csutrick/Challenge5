5213f87dc3177b15717bbc6b4169f4f2ae74d1ba
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-04-09 21:15:45 -0500

    Final touches

--- Develop/script.js
+++ Develop/script.js
@@ -1,50 +1,13 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 const dayJSObj = dayjs();
 var currentHour = 0;
 var testHour = 0;
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  
-
-
-
-
-
-
-
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
-
-
-
-// Update global currentHour var
 function updateHour() {
   currentHour = dayjs().hour()
   if (testHour > 0) {
     currentHour = testHour
     testHour++
-
-  }
+  };
   console.log(currentHour);
   boxColor();
   setTimeout(updateHour, 60000);
@@ -59,8 +22,8 @@ function loadSaved () {
   for (let i = 9; i <= 17; i++) {
     var txtCont = $('#txt' + i);
     txtCont.val(localStorage.getItem(i))
-  }
-}
+  };
+};
 
 function boxColor () {
   for (let i = 9; i <= 17; i++) {
@@ -72,11 +35,11 @@ function boxColor () {
       $('#hour-' + i).removeClass('past present future').addClass('future');
     }
   }
-}
+};
 
 
 
-$('#currentDay').text(dayjs().format('dddd[,] MMMM D'))
+$('#currentDay').text(dayjs().format('dddd[,] MMMM D'));
 updateHour();
 loadSaved();
 
